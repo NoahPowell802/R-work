7fcb3fb301828b37b7370c75ac0f3dee7832727e
NoahPowell802 <127001148+NoahPowell802@users.noreply.github.com>
2025-03-12 22:59:42 -0600

    Add files via upload

---
+++ STATS_HW_6.R
@@ -0,0 +1,71 @@
+# Q1
+  # P(X = 1.5) = P(Y = 1.5) because both would be = to 0
+  # P(X = 1.5) = 0 because binomial distributions are either 
+  # one or the other outcome never a half outcome.
+  # P(Y = 1.5) = 0 because any individual point on a normally
+  # distributed graph = 0
+
+# Q2
+  # Normal Distribution (Mostly Standard Normal)
+  # Z- and t-Tests
+  # General Hypothesis Testing
+  # Correlation
+  # (Ordinary Least Squares, single X, single Y) Linear Regression
+
+# Q3
+  # Decreasing covariance, increasing noise and standard deviation
+  # would lead to a decrease in correlation.
+
+# Q4
+  # The plots on wiki are all perfect and everything fits well
+  # but the real world is messy and not perfectly correlated
+  # or often influenced by an extremely large number of things.
+
+# Q5
+# just because food neophobia and masculinity are correlated
+# in the burger king data doesn't mean that someone scores
+# highly in food neophobia is necessarily more masculine.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
